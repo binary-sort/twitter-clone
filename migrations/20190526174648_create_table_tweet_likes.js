b5d3c32acb6787de59958043b750525791c2bced
Mayank Dubey <mayankdubey291@gmail.com>
2019-05-26 17:56:41 +0530

    Add migration to create likes table

---
+++ migrations/20190526174648_create_table_tweet_likes.js
@@ -0,0 +1,30 @@
+const Knex = require('knex');
+
+/**
+ *
+ *
+ * @param {Knex} knex
+ */
+exports.up = knex => {
+  return knex.schema
+    .createTable('tweet_likes', table => {
+      table.increments('id').primary();
+      table
+        .integer('tweet_id')
+        .references('id')
+        .inTable('tweet')
+        .notNullable()
+        .onDelete('CASCADE');
+      table
+        .integer('user_id')
+        .references('id')
+        .inTable('user')
+        .notNullable()
+        .onDelete('CASCADE');
+      table.timestamps(false, true);
+    })
+};
+
+exports.down = knex => {
+
+};
